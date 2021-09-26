005d720c23ae10829cc17db461a6ea8c312b5efd
MrKrrot <rafita.olguin@hotmail.com>
2021-09-25 21:32:21 -0500

    Add Herou needed config

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ app.use((req, res) => {
     res.status(404).json({error: 'Not Found'})
 })
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
